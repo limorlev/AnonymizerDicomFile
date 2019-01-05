4492719bfb4f56f917df37debdee7e2e5a97a936
Yaniv Harpaz <yaniv.harpaz@gmail.com>
2019-01-05 20:14:17 +0200

    set identify correctly

--- anonymizer_main.py
+++ anonymizer_main.py
@@ -1,14 +1,20 @@
 from file_type_classifier import FileTypeClassifier
 from image_file_type import ImageFileType
 import settings
+import pydicom
+
+
+def read_single_input_file(input_file):
+    dataset_file = pydicom.dcmread(input_file)
+    return dataset_file
 
 
 def main():
     print("Starting main")
     file_type_classifier = FileTypeClassifier()
 
-    file_type, ds_file = file_type_classifier.identify(settings.test_input_file,
-                                                       settings.modality_tag)
+    # file_type, ds_file = file_type_classifier.identify(settings.test_input_file,
+    #                                                    settings.modality_tag)
 
     image_file = ImageFileType()
 
@@ -19,6 +25,14 @@ def main():
     print(file_types_configuration['CT'])
     print(type(file_types_configuration['CT']['tags_to_anonymize']))
     print(file_types_configuration['CT']['tags_to_anonymize'])
+
+
+
+    input_dataset_file = read_single_input_file(settings.test_input_file)
+    # print(input_dataset_file)
+
+    input_file_type = FileTypeClassifier.identify(input_dataset_file)
+    print("input_file_type:", input_file_type)
 
     # tags_list = ImageFileType(file_type, configuration_file_path)
 
--- file_type_classifier.py
+++ file_type_classifier.py
@@ -1,9 +1,9 @@
 import pydicom
+import settings
 
 
 class FileTypeClassifier:
     @staticmethod
-    def identify(file_path, modality_tag):
-        ds_file = pydicom.dcmread(file_path)
-        file_type = ds_file[modality_tag].value
-        return file_type, ds_file
+    def identify(dataset_file):
+        file_type = dataset_file[settings.modality_tag].value
+        return file_type
